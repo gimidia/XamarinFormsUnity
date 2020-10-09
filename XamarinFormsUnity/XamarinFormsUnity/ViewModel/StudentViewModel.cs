26a59e2be1612a3febbab31bd199667e5325908f
gimidia <gimidia@hotmail.com>
2020-10-09 17:02:52 -0300

    criado pasta viewmodel e class StudentViewModel

---
+++ XamarinFormsUnity/XamarinFormsUnity/ViewModel/StudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using XamarinFormsUnity.Interface;
+
+namespace XamarinFormsUnity.ViewModel
+{
+    public class StudentViewModel
+    {
+        private readonly IStudentService _istudentservice;
+        public IEnumerable Students { get; set; }
+        public StudentViewModel(IStudentService istudentService)
+        {
+            _istudentservice = istudentService;
+            Students = _istudentservice.GetStudent();
+
+        }
+    }
+}
