87c7a16f732438e05b1ab72a34a26e1c76c42ee6
gimidia <gimidia@hotmail.com>
2020-10-09 16:50:45 -0300

    criado pasta model e class student

---
+++ XamarinFormsUnity/XamarinFormsUnity/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsUnity.Model
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+}
