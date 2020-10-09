b3bbfbd39a73398759be5c336a717fa3c0c7f08d
gimidia <gimidia@hotmail.com>
2020-10-09 17:09:51 -0300

    na pasta service criado class ViewModelLocator

---
+++ XamarinFormsUnity/XamarinFormsUnity/Service/ViewModelLocator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using XamarinFormsUnity.Interface;
+using XamarinFormsUnity.ViewModel;
+
+namespace XamarinFormsUnity.Service
+{
+    public class ViewModelLocator
+    {
+        private readonly IUnityContainer _container;
+        public ViewModelLocator()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IStudentService, StudentService>();
+            _container.RegisterInstance(typeof(StudentViewModel));
+        }
+
+        public StudentViewModel StudentViewModel
+        {
+            get { return _container.Resolve<StudentViewModel>(); }
+        }
+    }
+}
