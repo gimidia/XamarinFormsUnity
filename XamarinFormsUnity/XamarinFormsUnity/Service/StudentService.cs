362faac96064bf838946a750837eb1007f18ca5a
gimidia <gimidia@hotmail.com>
2020-10-09 16:55:04 -0300

    criado pasta service e class StudentService

---
+++ XamarinFormsUnity/XamarinFormsUnity/Service/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using XamarinFormsUnity.Interface;
+using XamarinFormsUnity.Model;
+
+namespace XamarinFormsUnity.Service
+{
+    public class StudentService : IStudentService
+    {
+        public IEnumerable GetStudent()
+        {
+            return new List<Student>{
+
+                new Student { Name = "Abhishek",Class="XII" },
+                new Student { Name = "Anbu",Class="XII" },
+                new Student { Name = "Akshay" ,Class="XII"},
+                new Student { Name = "Pramod" ,Class="XII"},
+                new Student { Name = "Raju" ,Class="XII"},
+                new Student { Name = "Sai" ,Class="XII"},
+
+           };
+        }
+    }
+}
