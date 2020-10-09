ac6315e54d77165d915af1aa8421e9f4070caf16
gimidia <gimidia@hotmail.com>
2020-10-09 16:47:58 -0300

    criado pasta interface e class interface

---
+++ XamarinFormsUnity/XamarinFormsUnity/Interface/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsUnity.Interface
+{
+    public interface IStudentService
+    {
+        IEnumerable GetStudent();
+    }
+}
